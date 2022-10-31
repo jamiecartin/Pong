496f12ca35bebee164969039397b97b4299df8b8
jaseyaws <jaseyaws@gmail.com>
2022-10-31 17:22:48 -0400

    Create game loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
-        Thread t1 = new Thread();
+        Thread t1 = new Thread(window);
         t1.start();
 
     }
--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -8,13 +11,31 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D)this.getGraphics();
 
+    }
+
+    public void update(double dt) {
+        System.out.println(1 / dt + " fps");
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
 
     }
 
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
-            //do whatever
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
+            try {
+                Thread.sleep(30);
+            } catch (Exception e) {
+
+            }
         }
     }
 }
