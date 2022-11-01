ac0fdc243b688ab4827ca5150b3b00448de68978
jaseyaws <jaseyaws@gmail.com>
2022-10-31 21:24:16 -0400

    Wrote code to establish user input handling

--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
     Graphics2D g2;
+    KL keyListener = new KL();
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -11,15 +13,22 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
         g2 = (Graphics2D)this.getGraphics();
 
     }
 
     public void update(double dt) {
-        System.out.println(1 / dt + " fps");
+        //System.out.println(1 / dt + " fps");
         g2.setColor(Color.BLACK);
         g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
 
+        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("The user is pressing the up arrow.");
+        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("Down");
+
+        }
     }
 
     public void run() {
